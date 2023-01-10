3dc3a9de27d2a425977439c5576f06306b4357b1
hyejinbaek <bhj4208@gmail.com>
2023-01-10 14:27:19 +0900

    baekjoon 1-7

---
+++ 230110_prac_01.py
@@ -0,0 +1,16 @@
+# 백준 1. 입출력과 사칙연산
+def step_10869():
+   a, b = map(int, input().split())
+   print(a+b)
+   print(a-b)
+   print(a*b)
+   print(a//b)
+   print(a%b)
+
+def step_10926():
+   id = input("")
+   print(id +"??!")
+
+
+if __name__ == '__main__':
+   step_10926()
