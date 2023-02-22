96324c0b9d948ca5c1408a03d412e079dacec2d7
hyejinbaek <bhj4208@gmail.com>
2023-02-22 23:07:18 +0900

    baekjoon time complexity 1-6

---
+++ 230222_prac_01.py
@@ -0,0 +1,33 @@
+def step_24262():
+    print("""1
+    0""")
+
+def step_24263():
+    n = int(input())
+    print(n)
+    print("1")
+
+def step_24264():
+    n = int(input())
+    n = n**2
+    print(n)
+    print("2")
+
+def step_24265():
+    n = int(input())
+    print((n-1)*(n)//2)
+    print("2")
+
+def step_24266():
+    n = int(input())
+    print((n)*(n)*(n))
+    print("3")
+
+def step_24267():
+    n = int(input())
+    print((n-2)*(n-1)*(n)//6)
+    print("3")
+    
+
+if __name__ == '__main__':
+    step_24267()
