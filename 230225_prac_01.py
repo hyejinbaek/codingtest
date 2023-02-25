a7662f640543f5350e8862bdf7c07c687eaf4c58
hyejinbaek <bhj4208@gmail.com>
2023-02-25 21:03:20 +0900

    baekjoon 12/6

---
+++ 230225_prac_01.py
@@ -0,0 +1,17 @@
+def hanoi(n, start, end):
+    if n == 1:
+        print("start end === ", start, end)
+        return
+    hanoi(n-1, start, 6-start-end) 
+    print(start,end)
+    hanoi(n-1, 6-start-end, end)
+
+def step_11729():
+    n = int(input())
+    print(2**n-1)
+    hanoi(n, 1, 3)
+
+
+
+if __name__ == '__main__':
+    step_11729()
