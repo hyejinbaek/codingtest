fc9640543279c424013c282548549538b225e4d2
hyejinbaek <bhj4208@gmail.com>
2025-02-20 08:16:08 +0900

    code modify

--- 2025/250220.py
+++ 2025/250220.py
@@ -1,10 +1,12 @@
 # 숫자 카드 (10815번)
 
 n = int(input())
-n_num = list(map(int, input().split()))
+# set으로 변환하여 시간 단축
+n_num = set(map(int, input().split()))
 
 m = int(input())
 m_num = list(map(int, input().split()))
 
 total = [1 if num in n_num else 0 for num in m_num]
 print(' '.join(map(str, total)))
+
